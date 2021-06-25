f062aaab612db2a60cef76db5949686159078b87
hrokr <alex.rook@gmail.com>
2021-06-24 23:58:58 -0400

    almost does the min

--- d6/d6.py
+++ d6/d6.py
@@ -1,31 +1,30 @@
 import click
 import random
 
-@click.group()
-def cli():
-    pass
+# @click.group()
+# def cli():
+#     pass
+
+@click.argument('ndx')
 
 @click.command()
-@click.argument('ndx')
 def parse(ndx):
     """CLI dice generator"""
     c = tuple(ndx.split('d'))
     num, d = c[0], c[1]
     click.echo(f"{num} {d}")
 
-@click.command()
-def gen(num, d):
-    """Random dice roll generator"""
-    for _ in range(num):
-        click.echo(f"{random.randint(1,d)}")
+# @click.command()
+# def gen(num, d):
+#     """Random dice roll generator"""
+#     for _ in range(num):
+#         click.echo(f"{random.randint(1,d)}")
 
 # cli.add_command(parse)
 # cli.add_command(gen)
 
-
-
 # @click.option('-s', '--sort', help="Return sorted results. Default: low to high")
 # #@click.option('-d', "--discard", help="discards roll(s). Default: low roll(s)")
 
-# if __name__ == '__main__':
-#     cli()
+if __name__ == '__main__':
+    parse()
