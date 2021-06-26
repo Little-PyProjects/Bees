6be2afbf3df5c16fe8a97a552379df9528827309
hrokr <alex.rook@gmail.com>
2021-06-26 18:15:33 -0400

    Change: refactored project sturcture

--- d6/setup.py
+++ d6/setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup
+from os import read
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        contents = req.read()
+        requirements = contents.split('\n')
+    return requirements
+
 
 setup(
     name='d6',
     version='0.0.2',
-    py_modules=['d6'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points=""""
+    packages=find_packages(),
+    include_packge_data=True,
+    install_requires=read_requirements(),
+    entry_points="""
         [console_scripts]
-        d6 = d6:cli
-    """,
+        d6 = d6.cli:cli
+    """
 )
