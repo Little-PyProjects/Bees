f52358f2a0ca18adaf3b0383a9edf9c53fde999a
hrokr <alex.rook@gmail.com>
2021-06-27 09:35:25 -0400

    Add parse.py

--- d6/d6/commands/parse.py
+++ d6/d6/commands/parse.py
@@ -2,12 +2,17 @@ import click
 
 @click.group(invoke_without_command=True, chain=True)
 def cli():
-    pass
+    """ d6 - The CLI dice roll generator. Base usage d6 {number of}{sided dice}.
+    e.g., d6 2d20 -- generates two 20 sided dice rolls
+    """
+    #parse()
+    click.echo(ndx)
+
 
 @cli.command()
 @click.argument('ndx')
 def parse(ndx):
-    """CLI dice generator"""
+    """{number of rolls}d{dice sides} eg 2d20"""
     c = tuple(ndx.split('d'))
     num, d = c[0], c[1]
     # click.echo(f"{num} {d}")
@@ -16,6 +21,7 @@ def parse(ndx):
 @cli.command()
 def sortin():
     pass
+
 # # @click.option('-s', '--sort', is_flag=True, help="Return sorted results. Default: low to high")
 # # def sort(sort):
 # #     if sort:
