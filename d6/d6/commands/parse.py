44e2e5ba13e27eff845f6c1c868c7aa0c7827b78
hrokr <alex.rook@gmail.com>
2021-06-29 22:53:52 -0400

    refactor parese. Prob not for the last time

--- d6/d6/commands/parse.py
+++ d6/d6/commands/parse.py
@@ -1,16 +1,25 @@
 import click
 
+class Context:
+    def __init__(self) -> None:
+        self.ndx
+
 @click.group(invoke_without_command=True, chain=True)
-def cli():
-    """ d6 - The CLI dice roll generator. Base usage d6 {number of}{sided dice}.
-    e.g., d6 2d20 -- generates two 20 sided dice rolls
-    """
-    #parse()
-    click.echo(ndx)
+@click.option('-s', '--sort', is_flag=True, help="Return sorted results. Default: low to high")
+# # def sort(sort):
+# #     if sort:
+# #         pass
+# #     click.echo(gen.sorted())
+
+@click.pass_context
+def cli(ctx, sort):
+    """ d6 - The CLI dice roll generator """
+    # parse()
+    ctx.obj = Context(ntx)
 
 
 @cli.command()
-@click.argument('ndx')
+@click.pass_context(ctx)
 def parse(ndx):
     """{number of rolls}d{dice sides} eg 2d20"""
     c = tuple(ndx.split('d'))
